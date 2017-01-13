713ec79bd5b3bc7fec2402349bb74cf2253d7234
Jacob Posner <jposner91@gmail.com>
2017-01-13 14:30:53 -0500

    Setting up initial project structure and git remote

---
+++ CustomAddressBook/AddViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddViewController.swift
+//  CustomAddressBook
+//
+//  Created by Jacob Posner on 1/13/17.
+//  Copyright © 2017 Kaizen Human. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddViewController : UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
