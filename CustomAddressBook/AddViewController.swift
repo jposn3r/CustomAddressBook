9a1104bc42bb7a3e03c7c2a4c47a9e0da4798a8c
Jacob Posner <jposner91@gmail.com>
2017-01-16 13:01:10 -0500

    Finished the layout of the add contact form

--- CustomAddressBook/AddViewController.swift
+++ CustomAddressBook/AddViewController.swift
@@ -9,20 +9,41 @@
 import Foundation
 import UIKit
 
-class AddViewController : UIViewController {
+class AddViewController : UIViewController, UITextFieldDelegate  {
     
     //Checking if I set it up properly for github.
     
     @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var addressField: UITextField!
     
     @IBAction func addButtonPressed(sender: UIButton) {
         print("Button Pressed Foo")
-        let person = Person(firstName: nameField.text!)
-        print("Created a person: \(person.firstName)")
+        
+        if let person = Person(firstName: nameField.text!) {
+            print("Created a person: \(person.firstName)")
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            print("Error creating Person")
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
     }
     
 }
